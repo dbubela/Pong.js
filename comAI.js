30aa4776ec26c11fbfe31b2038cc5cb80d4c0ffb
david.dev <69808811+dbubela@users.noreply.github.com>
2020-10-29 20:46:09 -0500

    Revert "Dbubela workflow notes added to /Pong.js/workflow notes/ (#3)"
    This reverts commit 6ddfdb84946ca8186a66b9348df168f2ce50a9b7.

--- comAI.js
+++
@@ -1,7 +0,0 @@
-//here we go with more calculations
-
-//easiest way to do this would be to have paddle track ball vertices and reflect the y values 
-com.y += ball.y - (com.y + com.height/2)
-//you will never win ever - add difficulty levels using small values <= 0.9
-let computerLevel = 0.1;
-// 0.0 = stupid > 1.0 = god tier
